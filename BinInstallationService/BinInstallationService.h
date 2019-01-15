21f72820510d0daefb14b28d6f2f5d73225a8db1
andre <andre@bottletop.com.au>
2019-01-15 19:13:31 +0700

    Initial Commit

---
+++ BinInstallationService/BinInstallationService.h
@@ -0,0 +1,19 @@
+//
+//  BinInstallationService.h
+//  BinInstallationService
+//
+//  Created by Andre on 15/01/19.
+//  Copyright © 2019 Andre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BinInstallationService.
+FOUNDATION_EXPORT double BinInstallationServiceVersionNumber;
+
+//! Project version string for BinInstallationService.
+FOUNDATION_EXPORT const unsigned char BinInstallationServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BinInstallationService/PublicHeader.h>
+
+
